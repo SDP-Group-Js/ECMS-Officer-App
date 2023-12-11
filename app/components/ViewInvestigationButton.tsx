75ade67d8de97ef81bdee0c88516e7d3753c6973
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2023-12-11 22:40:15 +0530

    Starting work on View page for specific investigations

--- app/components/ViewInvestigationButton.tsx
+++ app/components/ViewInvestigationButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 type InvestigationCardProps = {
@@ -10,8 +11,10 @@ const ViewInvestigationButton = ({
 }: InvestigationCardProps) => {
   return (
     <>
-      <button className="flex h-8 w-40 items-center justify-center rounded-lg border-2 border-emerald-500 bg-emerald-500 px-4 py-2 text-sm text-white transition-all hover:font-bold lg:h-10 lg:w-48 lg:text-base">
-        View Investigation
+      <button className="flex h-8 w-44 items-center justify-center rounded-lg border-2 border-emerald-500 bg-emerald-500 px-4 py-2 text-sm text-white transition-all hover:font-bold lg:h-10 lg:w-48 lg:text-base">
+        <Link href={`/viewInvestigations/${investigationId}`}>
+          View Investigation
+        </Link>
       </button>
     </>
   );
